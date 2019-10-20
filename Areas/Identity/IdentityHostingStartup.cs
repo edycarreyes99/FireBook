f56220c0eb2e7566ace845d3ab5ff3f563703732
Edycar Reyes <edycarreyes99@3619215.no-reply.drupal.org>
2019-10-20 15:42:15 -0600

    se extraen todas las vistas del Account y Account.Manage para posteriormente ser modificadas, todo trabajando correctamente hasta el momento

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using FireBook.Data;
+using FireBook.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FireBook.Areas.Identity.IdentityHostingStartup))]
+namespace FireBook.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
